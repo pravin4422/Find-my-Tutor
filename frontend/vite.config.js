5c38bf272486b58165ebe12e3304be68c4169b0f
Pravin.R <rpravin4422@gmail.com>
2025-12-12 11:06:14 +0530

    Change build output directory and plugins format
    Updated the output directory for the build and simplified the plugins array.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: '../public'
+    outDir: 'dist'  // ✔ Required for Vercel
   }
 })
